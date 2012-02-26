f4bbd8a6de6950dc20863e1730122cb1c2babb95
Tami Evnin <tami.evnin@gmail.com>
2012-02-25 20:17:45 -0500

    twitter and particles, without text

---
+++ ofxJSON_twitter/src/testApp.h
@@ -0,0 +1,43 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+
+#include "ofMain.h"
+#include "ofxJSONElement.h"
+#include "particle.h"
+
+
+class testApp : public ofBaseApp{
+	
+	public:
+		
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		
+		
+		ofTrueTypeFont  franklinBook;
+	
+		ofxJSONElement result;
+    
+        // particle stuff
+        vector <particle> particles;
+        
+        void audioRequested(float *input, int bufferSize, int nChannels);
+        float frequency;
+        float position;
+        float positionAdder;
+    
+        bool printHash;
+};
+
+#endif	
+
